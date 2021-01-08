aea0e6c16b11b2c0715af086a00b9b030fa3aae8
minakshiUI <vbhealth99@gmail.com>
2021-01-09 03:14:13 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { AppComponent } from './app.component';
-import { SakalPageComponent } from './sakal-page/sakal-page.component';
 import { HeaderComponent } from './header/header.component';
 import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SakalPageComponent,
     HeaderComponent
   ],
   imports: [
