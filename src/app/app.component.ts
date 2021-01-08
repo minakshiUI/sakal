dab6a39c1b8055d6f79a03162067d20c7d5b7626
minakshiUI <vbhealth99@gmail.com>
2021-01-08 15:00:10 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faCamera,
+  faMountain,
+  faAmbulance,
+  faUmbrellaBeach,
+  faGem,
+  faPrayingHands,
+  faGopuram,
+  faArchway,
+  faChild,
+  faHorse,
+  faEllipsisV,
+  faHeart,
+  faCommentDots,
+  faShare
+   } from '@fortawesome/free-solid-svg-icons';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +24,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  
   title = 'sakal';
+  faCamera=faCamera;
+  faAmbulance = faAmbulance;
+  faMountain=faMountain;
+  faUmbrellaBeach=faUmbrellaBeach;
+  faGem=faGem;
+  faPrayingHands=faPrayingHands;
+  faGopuram=faGopuram;
+  faArchway=faArchway;
+  faChild=faChild;
+  faHorse=faHorse;
+  faEllipsisV=faEllipsisV;
+  faHeart=faHeart;
+  faCommentDots=faCommentDots;
+  faShare=faShare;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
 import { AppComponent } from './app.component';
+import { SakalPageComponent } from './sakal-page/sakal-page.component';
+import { HeaderComponent } from './header/header.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SakalPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
